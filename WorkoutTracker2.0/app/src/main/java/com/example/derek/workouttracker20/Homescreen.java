2d4de01c7c715e237aca90151296afba531d4360
jw1762 <jw1762@txstate.edu>
2017-03-30 15:07:04 -0500

    Partial SmartLogin Implementation

--- WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/Homescreen.java
+++ WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/Homescreen.java
@@ -1,13 +1,28 @@
 package com.example.derek.workouttracker20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Homescreen extends AppCompatActivity {
+/*
+    public ImageButton Walk_Button;
+    public void WalkingLauncher()
+    {
+        Walk_Button = (ImageButton)findViewById(R.id.WalkButton);
+        Walk_Button.setOnClickListener()
+        {
+            Intent LaunchWalking = new Intent(Homescreen.this, WalkingActivity.class);
+            startActivity(LaunchWalking);
+        };
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+       // WalkingLauncher();//For implementation of butotn logic.
     }
 }
---
+++ WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/WalkingActivity.java
@@ -0,0 +1,11 @@
+package com.example.derek.workouttracker20;
+//Created by JW1762 on 3/30/2017.
+
+public class WalkingActivity
+{
+
+    void LaunchWalking()
+    {
+
+    }
+}
