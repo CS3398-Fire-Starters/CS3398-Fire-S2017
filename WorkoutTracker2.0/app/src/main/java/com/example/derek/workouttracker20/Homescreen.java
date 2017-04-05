1fd6cc74d387525a7accaa2eb9ddfcba5370cfa0
StevenB13 <s_b280@txstate.edu>
2017-04-05 16:32:33 -0500

    Linking settings tab

--- WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/Homescreen.java
+++ WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/Homescreen.java
@@ -8,9 +8,33 @@ import android.widget.ImageButton;
 
 public class Homescreen extends AppCompatActivity {
 
+    public ImageButton Settings_Button;
+
+    public void SettingsLauncher()
+    {
+        Settings_Button=(ImageButton)findViewById(R.id.SettingsButton);
+        Settings_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent LaunchSettings = new Intent(Homescreen.this, SettingsActivity.class);
+                startActivity(LaunchSettings);
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        SettingsLauncher();
     }
 }
