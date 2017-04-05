1402cba6d874489f3be3760f8c2184abe393b97a
Osawande <o_e16@txstate.edu>
2017-04-05 16:26:22 -0500

    other functions link

--- WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/Homescreen.java
+++ WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/Homescreen.java
@@ -8,9 +8,28 @@ import android.widget.ImageButton;
 
 public class Homescreen extends AppCompatActivity {
 
+    public ImageButton otherFunctions_button;
+
+    public void otherFunctions_launcher(){
+        otherFunctions_button = (ImageButton)findViewById(R.id.imageButton_otherFunctions);
+                otherFunctions_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent myIntent = new Intent(Homescreen.this, OtherFunctions.class);
+                                startActivity(myIntent);
+                    }
+                });
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        otherFunctions_launcher();
     }
+
+
+
+
 }
