48a03a82b7b208c191ec3fb9f3680aa45a176539
tqblocker95 <tqb2@txstate.edu>
2017-04-10 15:46:05 -0500

    Changes

--- WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/OtherFunctions.java
+++ WorkoutTracker2.0/app/src/main/java/com/example/derek/workouttracker20/OtherFunctions.java
@@ -1,13 +1,73 @@
 package com.example.derek.workouttracker20;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class OtherFunctions extends AppCompatActivity {
+public class OtherFunctions  extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_functions);
+
+        // Code to open any functions activity when a button is clicked
+        listView = (ListView) findViewById(R.id.list);
+        String[] values = new String[]{"BMI Calculator", "BMI Calculator",
+                "BMI Calculator", "BMI Calculator","BMI Calculator",
+                "BMI Calculator", "BMI Calculator",
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.activity_list_item, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), BMICalculator.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 1) {
+                    Intent myIntent = new Intent(view.getContext(), BMICalculator.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), BMICalculator.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 3) {
+                    Intent myIntent = new Intent(view.getContext(), BMICalculator.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 4) {
+                    Intent myIntent = new Intent(view.getContext(), BMICalculator.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 5) {
+                    Intent myIntent = new Intent(view.getContext(), BMICalculator.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 6) {
+                    Intent myIntent = new Intent(view.getContext(), BMICalculator.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+            }
+        });
     }
 }
+
